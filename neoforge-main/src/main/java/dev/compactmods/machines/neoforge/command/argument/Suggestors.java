d4cc1fa5b1b9c92094ffec8982fc229d22049635
Ted Senft <code@robotgryphon.com>
2023-12-16 13:36:22 -0500

    Add more functionality to give command
    9:34:20

--- neoforge-main/src/main/java/dev/compactmods/machines/neoforge/command/argument/Suggestors.java
+++ neoforge-main/src/main/java/dev/compactmods/machines/neoforge/command/argument/Suggestors.java
@@ -1,16 +1,24 @@
 package dev.compactmods.machines.neoforge.command.argument;
 
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import dev.compactmods.compactmachines.api.room.Rooms;
 import dev.compactmods.compactmachines.api.room.registration.IRoomRegistration;
+import dev.compactmods.machines.api.room.upgrade.RoomUpgrade;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Set;
 
 public class Suggestors {
+    public static final SuggestionProvider<CommandSourceStack> ROOM_TEMPLATES = (ctx, builder) ->
+            SharedSuggestionProvider.suggestResource(getRegistryValues(ctx, Rooms.TEMPLATE_REG_KEY), builder);
 
-    // FIXME
-//    public static final SuggestionProvider<CommandSourceStack> ROOM_UPGRADES = (ctx, builder) ->
-//            SharedSuggestionProvider.suggestResource(MachineRoomUpgrades.REGISTRY.keySet(), builder);
+    public static final SuggestionProvider<CommandSourceStack> ROOM_UPGRADES = (ctx, builder) ->
+            SharedSuggestionProvider.suggestResource(getRegistryValues(ctx, RoomUpgrade.REG_KEY), builder);
 
     public static final SuggestionProvider<CommandSourceStack> OWNED_ROOM_CODES = (ctx, builder) -> {
         final var owner = ctx.getSource().getPlayerOrException();
@@ -29,4 +37,10 @@ public class Suggestors {
 
         return SharedSuggestionProvider.suggest(codes, builder);
     };
+
+    private static <T> Set<ResourceLocation> getRegistryValues(CommandContext<CommandSourceStack> ctx, ResourceKey<Registry<T>> keyType) {
+        return ctx.getSource().getServer().registryAccess()
+                .registryOrThrow(keyType)
+                .keySet();
+    }
 }
--- neoforge-main/src/main/java/dev/compactmods/machines/neoforge/command/subcommand/CMGiveMachineSubcommand.java
+++ neoforge-main/src/main/java/dev/compactmods/machines/neoforge/command/subcommand/CMGiveMachineSubcommand.java
@@ -12,9 +12,13 @@ import dev.compactmods.machines.i18n.TranslationUtil;
 import dev.compactmods.machines.neoforge.command.argument.Suggestors;
 import dev.compactmods.machines.neoforge.config.ServerConfig;
 import dev.compactmods.machines.neoforge.machine.item.BoundCompactMachineItem;
+import dev.compactmods.machines.neoforge.machine.item.UnboundCompactMachineItem;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.arguments.ResourceLocationArgument;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import org.apache.logging.log4j.Logger;
 
@@ -26,19 +30,102 @@ public class CMGiveMachineSubcommand {
         final var subRoot = Commands.literal("give")
                 .requires(cs -> cs.hasPermission(ServerConfig.giveMachineLevel()));
 
-        subRoot.then(Commands.argument("player", EntityArgument.player())
+
+        // /cm give new [template]
+        subRoot.then(Commands.literal("new")
+                .then(Commands.argument("template", ResourceLocationArgument.id())
+                        .suggests(Suggestors.ROOM_TEMPLATES)
+                        .executes(CMGiveMachineSubcommand::giveNewMachineExecutor)));
+
+        // /cm give existing [room-code]
+        subRoot.then(Commands.literal("existing")
                 .then(Commands.argument("room", StringArgumentType.string())
                         .suggests(Suggestors.ROOM_CODES)
-                        .executes(CMGiveMachineSubcommand::giveMachine)));
+                        .executes(CMGiveMachineSubcommand::giveExistingRoomExecutor)));
+
+        // /cm give [player]
+        var giveSpecificPlayer = Commands.argument("player", EntityArgument.player());
+
+        // /cm give [player] new [template]
+        giveSpecificPlayer.then(Commands.literal("new")
+                        .then(Commands.argument("template", ResourceLocationArgument.id())
+                            .suggests(Suggestors.ROOM_TEMPLATES)
+                            .executes(CMGiveMachineSubcommand::giveNewMachineSpecificPlayer)));
+
+        // /cm give [player] existing [room-code]
+        giveSpecificPlayer.then(Commands.literal("existing")
+                .then(Commands.argument("room", StringArgumentType.string())
+                        .suggests(Suggestors.ROOM_CODES)
+                        .executes(CMGiveMachineSubcommand::giveExistingRoomSpecificPlayer)));
+
+        subRoot.then(giveSpecificPlayer);
+
+
+
 
         return subRoot;
     }
 
-    private static int giveMachine(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+
+    private static int giveNewMachineExecutor(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+        final var src = ctx.getSource();
+        final var player = src.getPlayerOrException();
+        final var templateId = ResourceLocationArgument.getId(ctx, "template");
+
+        createAndGiveNewMachine(src, templateId, player);
+
+        return 0;
+    }
+    
+    private static int giveNewMachineSpecificPlayer(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+        final var src = ctx.getSource();
+        final var player = EntityArgument.getPlayer(ctx, "player");
+        final var templateId = ResourceLocationArgument.getId(ctx, "template");
+
+        createAndGiveNewMachine(src, templateId, player);
+
+        return 0;
+    }
+
+    private static int giveExistingRoomExecutor(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+        final var src = ctx.getSource();
+        final var player = src.getPlayerOrException();
+        final var roomCode = StringArgumentType.getString(ctx, "room");
+
+        createAndGiveExistingRoom(roomCode, player, src);
+
+        return 0;
+    }
+
+    private static int giveExistingRoomSpecificPlayer(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
         final var src = ctx.getSource();
         final var player = EntityArgument.getPlayer(ctx, "player");
         final var roomCode = StringArgumentType.getString(ctx, "room");
 
+        createAndGiveExistingRoom(roomCode, player, src);
+
+        return 0;
+    }
+
+    private static void createAndGiveNewMachine(CommandSourceStack src, ResourceLocation templateId, ServerPlayer player) {
+
+        final var template = src.getServer().registryAccess()
+                .registryOrThrow(Rooms.TEMPLATE_REG_KEY)
+                .get(templateId);
+
+        if(template != null) {
+            final var item = UnboundCompactMachineItem.forTemplate(templateId, template);
+            if (!player.addItem(item)) {
+                src.sendFailure(TranslationUtil.command(CMCommands.CANNOT_GIVE_MACHINE));
+            } else {
+                src.sendSuccess(() -> TranslationUtil.command(CMCommands.MACHINE_GIVEN, player.getDisplayName()), true);
+            }
+        } else {
+            src.sendFailure(TranslationUtil.command(CMCommands.CANNOT_GIVE_MACHINE));
+        }
+    }
+
+    private static void createAndGiveExistingRoom(String roomCode, ServerPlayer player, CommandSourceStack src) {
         Rooms.registrar().get(roomCode).ifPresentOrElse(room -> {
             ItemStack newItem = BoundCompactMachineItem.createForRoom(roomCode);
             if (!player.addItem(newItem)) {
@@ -50,8 +137,6 @@ public class CMGiveMachineSubcommand {
             LOGGER.error("Error giving player a new machine block: room not found.");
             src.sendFailure(TranslationUtil.message(Messages.UNKNOWN_ROOM_CHUNK, roomCode));
         });
-
-        return 0;
     }
 }
 
