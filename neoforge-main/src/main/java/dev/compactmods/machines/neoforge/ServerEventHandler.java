61c7585b7e01c1f96a83a411c1f799dc58ec536d
Ted Senft <code@robotgryphon.com>
2023-12-23 01:59:30 -0500

    Fix API instances being set up too early

--- neoforge-main/src/main/java/dev/compactmods/machines/neoforge/ServerEventHandler.java
+++ neoforge-main/src/main/java/dev/compactmods/machines/neoforge/ServerEventHandler.java
@@ -8,6 +8,7 @@ import dev.compactmods.compactmachines.api.room.spawn.IRoomSpawnManagers;
 import dev.compactmods.machines.LoggingUtil;
 import dev.compactmods.machines.api.core.Constants;
 import dev.compactmods.machines.api.dimension.CompactDimension;
+import dev.compactmods.machines.api.dimension.MissingDimensionException;
 import dev.compactmods.machines.room.RoomApiInstance;
 import dev.compactmods.machines.room.RoomRegistration;
 import net.minecraft.server.level.ServerLevel;
@@ -15,6 +16,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.level.LevelEvent;
 import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class ServerEventHandler {
@@ -23,17 +25,7 @@ public class ServerEventHandler {
     public static void onServerAboutToStart(final ServerAboutToStartEvent server) {
         final var modLog = LoggingUtil.modLog();
 
-        modLog.debug("Setting up room API instances.");
-        final IRoomRegistrar registrar = new RoomRegistration();
-        final IRoomOwners owners = null;
-        final IRoomSpawnManagers spawnManager = null;
-        final IRoomChunkManager chunkManager = null; // new GraphChunkManager();
-
-        //noinspection UnstableApiUsage
-        RoomApi.INSTANCE = new RoomApiInstance(registrar, owners, spawnManager, chunkManager);
-        modLog.debug("Completed setting up room API instances.");
-
-        modLog.debug("Starting addon scan and injection for server startup.");
+        // modLog.debug("Starting addon scan and injection for server startup.");
 //        CompactMachines.getAddons().forEach(addon -> {
 //            final Supplier<IRoomOwnerLookup> ownerLookup = ForgeCompactRoomProvider::instance;
 //            final Supplier<IRoomSpawnLookup> spawnLookup = ForgeCompactRoomProvider::instance;
@@ -51,9 +43,28 @@ public class ServerEventHandler {
     }
 
     @SubscribeEvent
+    public static void onServerStarting(final ServerStartingEvent server) {
+        final var modLog = LoggingUtil.modLog();
+
+        try {
+            modLog.debug("Setting up room API instances.");
+            final IRoomRegistrar registrar = RoomRegistration.forServer(server.getServer());
+            final IRoomOwners owners = null;
+            final IRoomSpawnManagers spawnManager = null;
+            final IRoomChunkManager chunkManager = null; // new GraphChunkManager();
+
+            //noinspection UnstableApiUsage
+            RoomApi.INSTANCE = new RoomApiInstance(registrar, owners, spawnManager, chunkManager);
+            modLog.debug("Completed setting up room API instances.");
+        } catch (MissingDimensionException e) {
+            modLog.fatal("Failed to set up room API instance; dimension error.", e);
+        }
+    }
+
+    @SubscribeEvent
     public static void onWorldLoaded(final LevelEvent.Load evt) {
         if (evt.getLevel() instanceof ServerLevel compactDim && compactDim.dimension().equals(CompactDimension.LEVEL_KEY)) {
-        // FIXME Room upgrade initialization
+            // FIXME Room upgrade initialization
 //            final var levelUpgrades = RoomUpgradeManager.get(compactDim);
 //            final var roomInfo = CompactRoomProvider.instance(compactDim);
 //
