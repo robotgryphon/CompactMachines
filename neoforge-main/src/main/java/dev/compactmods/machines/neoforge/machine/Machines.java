8c8741a2fb336231853080f8c6f850506afa3903
Ted Senft <code@robotgryphon.com>
2024-02-03 22:19:36 -0500

    Switch machines to an interface

--- neoforge-main/src/main/java/dev/compactmods/machines/neoforge/machine/Machines.java
+++ neoforge-main/src/main/java/dev/compactmods/machines/neoforge/machine/Machines.java
@@ -18,37 +18,36 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import java.util.function.Supplier;
 
 @SuppressWarnings("removal")
-public class Machines {
+public interface Machines {
     // TODO: Metal material replacement
-    static final BlockBehaviour.Properties MACHINE_BLOCK_PROPS = BlockBehaviour.Properties
+    BlockBehaviour.Properties MACHINE_BLOCK_PROPS = BlockBehaviour.Properties
             .of()
             .strength(8.0F, 20.0F)
             .requiresCorrectToolForDrops();
 
-    static final Supplier<Item.Properties> MACHINE_ITEM_PROPS = Item.Properties::new;
+    Supplier<Item.Properties> MACHINE_ITEM_PROPS = Item.Properties::new;
 
-    public static final DeferredBlock<UnboundCompactMachineBlock> UNBOUND_MACHINE_BLOCK = Registries.BLOCKS.register("new_machine", () ->
+    DeferredBlock<UnboundCompactMachineBlock> UNBOUND_MACHINE_BLOCK = Registries.BLOCKS.register("new_machine", () ->
             new UnboundCompactMachineBlock(MACHINE_BLOCK_PROPS));
 
-    public static final DeferredBlock<BoundCompactMachineBlock> MACHINE_BLOCK = Registries.BLOCKS.register("machine", () ->
+    DeferredBlock<BoundCompactMachineBlock> MACHINE_BLOCK = Registries.BLOCKS.register("machine", () ->
             new BoundCompactMachineBlock(MACHINE_BLOCK_PROPS));
 
-    public static final DeferredItem<BoundCompactMachineItem> BOUND_MACHINE_BLOCK_ITEM = Registries.ITEMS.register("machine",
+    DeferredItem<BoundCompactMachineItem> BOUND_MACHINE_BLOCK_ITEM = Registries.ITEMS.register("machine",
             () -> new BoundCompactMachineItem(MACHINE_ITEM_PROPS.get()));
 
-
-    public static final DeferredItem<UnboundCompactMachineItem> UNBOUND_MACHINE_BLOCK_ITEM = Registries.ITEMS.register("new_machine",
+    DeferredItem<UnboundCompactMachineItem> UNBOUND_MACHINE_BLOCK_ITEM = Registries.ITEMS.register("new_machine",
             () -> new UnboundCompactMachineItem(MACHINE_ITEM_PROPS.get()));
 
-    public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<UnboundCompactMachineEntity>> UNBOUND_MACHINE_ENTITY = Registries.BLOCK_ENTITIES.register(MachineIds.UNBOUND_MACHINE_ENTITY.getPath(), () ->
+    DeferredHolder<BlockEntityType<?>, BlockEntityType<UnboundCompactMachineEntity>> UNBOUND_MACHINE_ENTITY = Registries.BLOCK_ENTITIES.register(MachineIds.UNBOUND_MACHINE_ENTITY.getPath(), () ->
             BlockEntityType.Builder.of(UnboundCompactMachineEntity::new, UNBOUND_MACHINE_BLOCK.get())
                     .build(null));
 
-    public static final DeferredHolder<BlockEntityType<?>,BlockEntityType<BoundCompactMachineBlockEntity>> MACHINE_ENTITY = Registries.BLOCK_ENTITIES.register(MachineIds.BOUND_MACHINE_ENTITY.getPath(), () ->
+    DeferredHolder<BlockEntityType<?>,BlockEntityType<BoundCompactMachineBlockEntity>> MACHINE_ENTITY = Registries.BLOCK_ENTITIES.register(MachineIds.BOUND_MACHINE_ENTITY.getPath(), () ->
             BlockEntityType.Builder.of(BoundCompactMachineBlockEntity::new, MACHINE_BLOCK.get())
                     .build(null));
 
-    public static void prepare() {
+    static void prepare() {
 
     }
 }
