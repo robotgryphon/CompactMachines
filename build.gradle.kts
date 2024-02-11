2e3f722de43dcc44ce2654fc7ce042501dd13240
Ted Senft <code@robotgryphon.com>
2024-02-11 10:07:16 -0500

    Manifest issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    // id("net.neoforged.gradle.userdev") version("7.0.74") apply(false)
+    id("net.neoforged.gradle.common") version ("7.0.93")
     id("org.jetbrains.gradle.plugin.idea-ext") version ("1.1.7")
 }
